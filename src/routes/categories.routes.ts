cb029a402f30acb5bb96b43097ea7d6e701ca304
Rodrigo Viana <roviol@gmail.com>
2022-11-17 20:49:54 -0300

    refact: applying SOLID principles

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,22 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post('/', (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
 
-  if (categoryAlreadyExists) {
-    return response.status(400).json({ error: 'Category already exists!' });
-  }
-
-  categoriesRepository.create({ name, description });
+  createCategoryService.execute({ name, description });
 
   return response.status(201).send();
 });
 
-categoriesRoutes.get('/', (request, response) => {
+categoriesRoutes.get("/", (request, response) => {
   const all = categoriesRepository.list();
 
   return response.json(all);
